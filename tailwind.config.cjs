448be42773dd562cf3a8f6ef93821e427c0963a8
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-16 20:42:37 +0100

    Added colors and fonts.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./main.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        softOrange: "hsl(35, 77%, 62%)",
+        softRed: "hsl(5, 85%, 63%)",
+        // Neutral
+        offWhite: "hsl(36, 100%, 99%)",
+        grayishBlue: "hsl(233, 8%, 79%)",
+        darkGrayishBlue: "hsl(236, 13%, 42%)",
+        veryDarkBlue: "hsl(240, 100%, 5%)",
+      },
+    },
   },
   plugins: [],
 };
